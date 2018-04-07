0091470fd9ea3ca188a80e2c9f9fadfdc1c4c855
GabrielXia <Ga.Xiajin@gmail.com>
2018-04-07 20:38:31 +0200

    init

---
+++ ImagenetTinyOutlier.py
@@ -0,0 +1,36 @@
+import os
+import torchvision.datasets as datasets
+from torchvision.datasets.folder import default_loader, is_image_file
+from make_outlier import read_table
+
+
+def make_dataset(dir, class_to_idx, map):
+    images = []
+    dir = os.path.expanduser(dir)
+    for target in sorted(os.listdir(dir)):
+        d = os.path.join(dir, target)
+        if not os.path.isdir(d):
+            continue
+
+        for root, _, fnames in sorted(os.walk(d)):
+            for fname in sorted(fnames):
+                if is_image_file(fname):
+                    path = os.path.join(root, fname)
+                    l_outlier = map[fname][1]
+                    item = (path, class_to_idx[l_outlier])
+                    images.append(item)
+
+    return images
+
+
+class ImagenetTinyOutlier(datasets.ImageFolder):
+    def __init__(self, root, ratio=0.1, transform=None, target_transform=None,
+                 loader=default_loader):
+        super(ImagenetTinyOutlier, self).__init__(root, transform, target_transform, loader)
+        self.ratio = ratio
+        self.root = root
+        map = read_table(root)
+        self.samples = make_dataset(root, self.class_to_idx, map)
+        self.imgs = self.samples
+
+dir = './tiny-imagenet-200/train/'
---
+++ make_outlier.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import csv
+
+from torchvision.datasets.folder import is_image_file
+
+
+# name, is_outlier, outlier_type, true_type
+def write_outlier(image_dir, ratio=0.1):
+    type_list = [i for i in os.listdir(image_dir) if os.path.isdir(image_dir + i + '/images/')]
+    for type in type_list:
+        if os.path.isdir(image_dir + type + '/images/'):
+            images = [i for i in os.listdir(image_dir + type + '/images/') if is_image_file(i)]
+            size_list = len(images)
+            outlier_num = int(size_list * ratio)
+            outlier_index = np.random.choice(size_list, outlier_num, replace=False)
+            outlier_label = np.random.choice([i for i in type_list if i != type], outlier_num, replace=True)
+            with open(image_dir + type + '/' + type + '_outlier_' + str(float(ratio)) + '.csv', 'wb') as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=',',
+                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                count = 0
+                for i, im in enumerate(images):
+                    if i not in outlier_index:
+                        spamwriter.writerow([im, 0, type, type])
+                    else:
+                        spamwriter.writerow([im, 1, outlier_label[count], type])
+                        count += 1
+
+def read_table(image_dir, ratio=0.1):
+    map = {}
+    r = float(ratio)
+    for type in os.listdir(image_dir):
+        if os.path.isdir(image_dir + type + '/images/'):
+            with open(image_dir + type + '/' + type + '_outlier_' + str(r) + '.csv', 'rb') as csvfile:
+                spamwriter = csv.reader(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                for row in spamwriter:
+                    map[row[0]] = (bool(row[1]), row[2], row[3])
+    return map
+
+
+def main():
+    dir = './tiny-imagenet-200/train/'
+    for i in [0, 0.1, 0.2, 0.3]:
+        write_outlier(dir, i)
+
+main()
